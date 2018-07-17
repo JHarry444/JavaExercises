cb9850825643178462eb01df05d795dd61a6e84a
JHarry444 <jordan.harrison@academytrainee.com>
2018-07-17 14:24:26 +0100

    Forgot to do this for a while.

--- Academy/src/Level2/Exercise2.java
+++ Academy/src/Level2/Exercise2.java
@@ -1,5 +1,9 @@
 package Level2;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
 import java.util.*;
 
 public class Exercise2 {
@@ -19,7 +23,7 @@ public class Exercise2 {
         for (Person person : people) {
             System.out.println(person.toString());
         }*/
-    List<Vehicle> vehicles = new ArrayList<>();
+/*    List<Vehicle> vehicles = new ArrayList<>();
     vehicles.add(new Car("1", 1));
     vehicles.add(new Car("2", 2));
     vehicles.add(new Car("3", 3));
@@ -29,6 +33,32 @@ public class Exercise2 {
     vehicles.add(new Motorbike("7", 1));
     vehicles.add(new Motorbike("8", 2));
     vehicles.add(new Motorbike("9", 3));
-    System.out.println(new Garage(vehicles).calcTotalPrice());
+    Garage garage = new Garage(vehicles);
+    System.out.println(garage);
+    garage.removeVehicleByType(Car.class);*/
+//        PaintWizard.findLeastWasteful(1500);
+//        PaintWizard.findCheapest(2000);
+        Person ifti = new Person("Ifti Khan", 28, "Trainee Consultant");
+        Person dale = new Person("Dale Carr", 28, "Consultant Trainer");
+        Person assad = new Person("Assad Hussein", 22, "Trainee Consultant");
+        Person nelvin = new Person("Nelvin Jones", 22, "Trainee Consultant");
+        Person jordan = new Person("Jordan Harrison", 24, "Trainee Consultant");
+        List<Person> people = new ArrayList<Person>();
+        people.add(ifti);
+        people.add(dale);
+        people.add(assad);
+        people.add(nelvin);
+        people.add(jordan);
+        for (Person person : people) {
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(new File("C:\\Users\\Admin" + person.name)));
+                writer.write(person.toString());
+            } catch (Throwable e) {
+                System.out.println(e.getMessage());
+            } finally {
+                writer
+            }
+        }
     }
+
 }
--- Academy/src/Level2/PaintWizard.java
+++ Academy/src/Level2/PaintWizard.java
@@ -1,8 +1,12 @@
 package Level2;
 
+import java.util.*;
+
 public enum PaintWizard {
 
-    CHEAPOMAX (20, 19.99, 10), AVERAGEJOES(15, 17.99, 11), DULUXOUROUSPAINTS(10, 25, 20);
+    CHEAPOMAX(20, 19.99, 10) {
+
+    }, AVERAGEJOES(15, 17.99, 11), DULUXOUROUSPAINTS(10, 25, 20);
 
     private int litres;
     private double price;
@@ -12,5 +16,43 @@ public enum PaintWizard {
         this.litres = litres;
         this.price = price;
         this.coverage = coverage;
+    }
+
+    public int getTotalCoverage() {
+        return this.litres * this.coverage;
+    }
+
+    private double priceForRoom(int size) {
+        return  Math.round(size / this.getTotalCoverage()) * this.price; //tins needed * price
+    }
+
+    public static PaintWizard findLeastWasteful(int squareFootage) {
+        int cheapoWaste = CHEAPOMAX.getTotalCoverage() > squareFootage ? CHEAPOMAX.getTotalCoverage() % squareFootage : squareFootage % CHEAPOMAX.getTotalCoverage();
+        int averageWaste = AVERAGEJOES.getTotalCoverage() > squareFootage ? AVERAGEJOES.getTotalCoverage() % squareFootage : squareFootage % AVERAGEJOES.getTotalCoverage();
+        int duluxWaste = DULUXOUROUSPAINTS.getTotalCoverage() > squareFootage ? DULUXOUROUSPAINTS.getTotalCoverage() % squareFootage : squareFootage % DULUXOUROUSPAINTS.getTotalCoverage();
+        if (cheapoWaste < averageWaste && cheapoWaste < duluxWaste) {
+            return CHEAPOMAX;
+        } else if (averageWaste < cheapoWaste && averageWaste < duluxWaste) {
+            return AVERAGEJOES;
+        } else if (duluxWaste < cheapoWaste && duluxWaste < averageWaste) {
+            return  DULUXOUROUSPAINTS;
+        } else {
+            return CHEAPOMAX;
+        }
+    }
+
+    public static PaintWizard findCheapest(int size) {
+        double cheapPrice = CHEAPOMAX.priceForRoom(size);
+        double averagePrice = AVERAGEJOES.priceForRoom(size);
+        double duluxPrice = DULUXOUROUSPAINTS.priceForRoom(size);
+        if (cheapPrice < averagePrice && cheapPrice < duluxPrice) {
+            return CHEAPOMAX;
+        } else if (averagePrice < cheapPrice && averagePrice < duluxPrice) {
+            return AVERAGEJOES;
+        } else if (duluxPrice < cheapPrice && duluxPrice < averagePrice) {
+            return DULUXOUROUSPAINTS;
+        } else {
+            return CHEAPOMAX;
+        }
     }
 }

f07d331e6b303506761fa00d079590fce4400651
JHarry444 <jordan.harrison@academytrainee.com>
2018-07-17 10:36:00 +0100

    Hope this works. 2.0

--- Academy/src/Level2/PaintWizard.java
+++ Academy/src/Level2/PaintWizard.java
@@ -1,6 +1,16 @@
 package Level2;
 
-public class PaintWizard {
+public enum PaintWizard {
 
+    CHEAPOMAX (20, 19.99, 10), AVERAGEJOES(15, 17.99, 11), DULUXOUROUSPAINTS(10, 25, 20);
 
+    private int litres;
+    private double price;
+    private int coverage;
+
+    PaintWizard(int litres, double price, int coverage) {
+        this.litres = litres;
+        this.price = price;
+        this.coverage = coverage;
+    }
 }
